26ae4251a177654c325b4aa13db224fdf9ad24a5
Kuroikuma <odiseoj676@gmail.com>
2022-03-23 14:55:03 -0600

    feat: add component card

--- src/app/App.js
+++ src/app/App.js
@@ -5,6 +5,7 @@ import { Robot } from './components/robot'
 import { Feature } from './components/feature'
 import { Video } from './components/video'
 import { Services } from './components/services'
+import { Card } from './components/card'
 
 export function App() {
   return (
@@ -14,6 +15,7 @@ export function App() {
       <Feature />
       <Video />
       <Services />
+      <Card />
     </div>
   )
 }
---
+++ src/app/components/card.jsx
@@ -0,0 +1,20 @@
+import '../css/card.css'
+
+export const Card = () => {
+  return (
+    <div className="card-wrapper">
+      <div className="card-wrap">
+        <div className="card__content">
+          <h2>Increase your sales by analyzing your colleced data!</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Blandit est
+            ac nulla faucibus proin nisl augue. Vestibulum sem scelerisque
+            suspendisse praesent pretium non. At mattis bibendum ut sed
+            praesent. Nam at id elementum gravida.
+          </p>
+          <button>Learn More...</button>
+        </div>
+      </div>
+    </div>
+  )
+}
