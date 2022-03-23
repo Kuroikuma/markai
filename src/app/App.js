2e4b49eca3f7027e35b22c64e576e8d0fb6504ca
Kuroikuma <odiseoj676@gmail.com>
2022-03-22 23:36:38 -0600

    feat: add component feature

--- src/app/App.js
+++ src/app/App.js
@@ -2,12 +2,14 @@ import './css/App.css'
 import { Home } from './container/home'
 import React from 'react'
 import { Robot } from './components/robot'
+import { Feature } from './components/feature'
 
 export function App() {
   return (
     <div className="App">
       <Home />
       <Robot />
+      <Feature />
     </div>
   )
 }
